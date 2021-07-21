3882c63b8a8feac5299ae946da0b7b7f2430b4db
Cybernelsk8 <nelson.o.vasquez@gmail.com>
2021-07-21 16:46:10 -0600

    modificacion de la imagen de la pantalla principal y actualizacion de favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,20 @@
 <x-app-layout>
-    <section class=" bg-cover" style="background-image: url({{asset('img/home/netflix-3733812_1920.jpg')}})">
+    <section class=" bg-cover h-screen" style="background-image: url({{asset('img/home/netflix-3733812_1920.jpg')}});">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-36">
             <div class="w-full md:w-3/4 lg:w-1/2 bg-black bg-opacity-70 rounded-md shadow-md p-4">
                 <h1 class="text-white font-bold text-4xl text-center">Mira tu pelicula favorita en MuniMovies</h1>
                 <p class="text-white text-lg mt-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, odio! Reprehenderit dolores quo placeat, repudiandae id expedita praesentium quasi sunt molestiae facilis suscipit veniam.</p>
-            
+
                 @livewire('search')
 
             </div>
 
-            
+
         </div>
     </section>
 
     <section>
         @livewire('home.movies');
     </section>
-    
+
 </x-app-layout>
